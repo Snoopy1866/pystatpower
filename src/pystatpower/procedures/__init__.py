98530824f95d7c397f8c96820e0863b1bea96b6d
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-09-22 23:28:29 +0800

    refactor(one_proportion): all
    根据 base.py 重构代码

--- src/pystatpower/procedures/__init__.py
+++ src/pystatpower/procedures/__init__.py
@@ -1,3 +1,4 @@
 # 使用 from pystatpower.procedures import * 导入模块时，添加别名
 
-from pystatpower.procedures import one_sample_proportion, one_sample_proportion as ospp
+from pystatpower.procedures import one_proportion as ospp
+from pystatpower.procedures import two_proportion as tspp
