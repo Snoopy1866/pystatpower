a26723242ba261f43183b80e5506f8569319908e
Firefly <45487685+Snoopy1866@users.noreply.github.com>
2026-04-09 11:03:29 +0800

    fix

--- src/pystatpower/models/correlation/difference.py
+++ src/pystatpower/models/correlation/difference.py
@@ -1,4 +1,4 @@
-from math import log
+from math import log, sqrt
 
 from scipy.stats import norm
 
@@ -22,3 +22,29 @@ def size(alpha: float, power: float, null_correlation: float, correlation: float
         + 3
     )
     return float(size)
+
+
+def power(alpha: float, null_correlation: float, correlation: float, size: float) -> float:
+    """Calculate power of correlation difference test.
+
+    Args:
+        alpha (float): Significance level.
+        null_correlation (float): Correlation coefficient under null hypothesis.
+        correlation (float): Correlation coefficient under alternative hypothesis.
+        size (float): Sample size.
+
+    Returns:
+        power(float): Power of correlation difference test.
+    """
+
+    null_zeta = 1 / 2 * log((1 + null_correlation) / (1 - null_correlation))
+    zeta = 1 / 2 * log((1 + correlation) / (1 - correlation))
+
+    power = 1 - norm.cdf(
+        norm.ppf(1 - alpha / 2)
+        - (abs(zeta - null_zeta) + 1 / 2 * abs(correlation - null_correlation) / (size - 1)) * sqrt(size - 3)
+    )
+
+    a = norm.ppf(1 - alpha / 2)
+    b = norm.cdf(1.9599)
+    return float(power)
--- tests/test_models/test_correlation/test_difference.py
+++ tests/test_models/test_correlation/test_difference.py
@@ -2,7 +2,7 @@ from math import ceil
 
 import pytest
 
-from pystatpower.models.correlation.difference import size
+from pystatpower.models.correlation.difference import power, size
 
 
 @pytest.mark.parametrize(
@@ -18,3 +18,14 @@ from pystatpower.models.correlation.difference import size
 )
 def test_size(alpha: float, power: float, null_correlation: float, correlation: float, expected: float):
     assert ceil(size(alpha, power, null_correlation, correlation)) == expected
+
+
+@pytest.mark.parametrize(
+    "alpha, null_correlation, correlation, size, expected",
+    [
+        (0.05, 0.5, 0.2, 24, 0.98),
+        (0.05, 0.5, 0.3, 24, 0.98),
+    ],
+)
+def test_power(alpha: float, null_correlation: float, correlation: float, size: float, expected: float):
+    assert round(power(alpha, null_correlation, correlation, size), 4) == expected
