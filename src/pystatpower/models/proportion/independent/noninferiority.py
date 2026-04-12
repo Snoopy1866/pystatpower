c2c714197789dd8391ebd048cf4e95aa13c12607
Firefly <45487685+Snoopy1866@users.noreply.github.com>
2026-04-12 21:10:41 +0800

    update

--- src/pystatpower/models/proportion/independent/noninferiority.py
+++ src/pystatpower/models/proportion/independent/noninferiority.py
@@ -1,9 +1,399 @@
-from math import sqrt
+from math import ceil, sqrt
 
+from scipy.optimize import brentq
 from scipy.stats import norm
 
-from ...._constant import LOWER_LIMIT_OF_SAMPLE_SIZE
-from ...._restrict import Restrict
+from ...._constant import SAMPLE_SIZE_SEARCH_MAX
+
+
+def _power_pooled(
+    treatment_proportion: float,
+    reference_proportion: float,
+    magin: float,
+    treatment_size: float,
+    reference_size: float,
+    alpha: float = 0.05,
+) -> float:
+    pooled_proportion = (treatment_size * treatment_proportion + reference_size * reference_proportion) / (
+        treatment_size + reference_size
+    )
+    power = 1 - norm.cdf(
+        (
+            norm.ppf(1 - alpha)
+            * sqrt(pooled_proportion * (1 - pooled_proportion) * (1 / treatment_size + 1 / reference_size))
+            - abs(treatment_proportion - reference_proportion - magin)
+        )
+        / sqrt(
+            treatment_proportion * (1 - treatment_proportion) / treatment_size
+            + reference_proportion * (1 - reference_proportion) / reference_size
+        )
+    )
+    return float(power)
+
+
+def _power_pooled_cc(
+    treatment_proportion: float,
+    reference_proportion: float,
+    magin: float,
+    treatment_size: float,
+    reference_size: float,
+    alpha: float = 0.05,
+) -> float:
+    pooled_proportion = (treatment_size * treatment_proportion + reference_size * reference_proportion) / (
+        treatment_size + reference_size
+    )
+    power = 1 - norm.cdf(
+        (
+            norm.ppf(1 - alpha)
+            * sqrt(pooled_proportion * (1 - pooled_proportion) * (1 / treatment_size + 1 / reference_size))
+            - abs(
+                treatment_proportion - reference_proportion - magin - 1 / 2 * (1 / treatment_size + 1 / reference_size)
+            )
+        )
+        / sqrt(
+            treatment_proportion * (1 - treatment_proportion) / treatment_size
+            + reference_proportion * (1 - reference_proportion) / reference_size
+        )
+    )
+    return float(power)
+
+
+def _power_unpooled(
+    treatment_proportion: float,
+    reference_proportion: float,
+    magin: float,
+    treatment_size: float,
+    reference_size: float,
+    alpha: float = 0.05,
+) -> float:
+    power = 1 - norm.cdf(
+        norm.ppf(1 - alpha)
+        - abs(treatment_proportion - reference_proportion - magin)
+        / sqrt(
+            treatment_proportion * (1 - treatment_proportion) / treatment_size
+            + reference_proportion * (1 - reference_proportion) / reference_size
+        )
+    )
+    return float(power)
+
+
+def _power_unpooled_cc(
+    treatment_proportion: float,
+    reference_proportion: float,
+    magin: float,
+    treatment_size: float,
+    reference_size: float,
+    alpha: float = 0.05,
+) -> float:
+    power = 1 - norm.cdf(
+        norm.ppf(1 - alpha)
+        - abs(treatment_proportion - reference_proportion - magin - 1 / 2 * (1 / treatment_size + 1 / reference_size))
+        / sqrt(
+            treatment_proportion * (1 - treatment_proportion) / treatment_size
+            + reference_proportion * (1 - reference_proportion) / reference_size
+        )
+    )
+    return float(power)
+
+
+def _power(
+    treatment_proportion: float,
+    reference_proportion: float,
+    magin: float,
+    treatment_size: float,
+    reference_size: float,
+    alpha: float = 0.05,
+    pooled: bool = False,
+    continuity_correction: bool = False,
+) -> float:
+    if pooled:
+        if continuity_correction:
+            return _power_pooled_cc(
+                treatment_proportion, reference_proportion, magin, treatment_size, reference_size, alpha
+            )
+        else:
+            return _power_pooled(
+                treatment_proportion, reference_proportion, magin, treatment_size, reference_size, alpha
+            )
+    else:
+        if continuity_correction:
+            return _power_unpooled_cc(
+                treatment_proportion, reference_proportion, magin, treatment_size, reference_size, alpha
+            )
+        else:
+            return _power_unpooled(
+                treatment_proportion, reference_proportion, magin, treatment_size, reference_size, alpha
+            )
+
+
+def solve_power(
+    treatment_proportion: float,
+    reference_proportion: float,
+    margin: float,
+    treatment_size: float,
+    reference_size: float,
+    alpha: float = 0.05,
+    pooled: bool = False,
+    continuity_correction: bool = False,
+) -> float:
+    """Calculate the power of the non-inferiority test between two independent proportions.
+
+    Args:
+        treatment_proportion (float): Treatment proportion.
+        reference_proportion (float): Reference proportion.
+        margin (float): Non-inferiority margin, which should be negative when higher is better, otherwise positive.
+        treatment_size (float): Treatment sample size.
+        reference_size (float): Reference sample size.
+        alpha (float, optional): One-sided significance level. Default is 0.05.
+        pooled (bool, optional): Whether or not the pooled method is used. Default is False.
+        continuity_correction (bool, optional): Whether or not the continuity correction is used. Default is False.
+
+    Returns:
+        power(float): Power of the test.
+    """
+
+    power = _power(
+        treatment_proportion,
+        reference_proportion,
+        margin,
+        treatment_size,
+        reference_size,
+        alpha,
+        pooled,
+        continuity_correction,
+    )
+    return power
+
+
+def solve_size(
+    treatment_proportion: float,
+    reference_proportion: float,
+    margin: float,
+    ratio: float = 1,
+    alpha: float = 0.05,
+    power: float = 0.80,
+    pooled: bool = False,
+    continuity_correction: bool = False,
+) -> tuple[int, int]:
+    """Estimate the sample size required for the non-inferiority test between two independent proportions.
+
+    Args:
+        treatment_proportion (float): Treatment proportion.
+        reference_proportion (float): Reference proportion.
+        margin (float): Non-inferiority margin, which should be negative when higher is better, otherwise positive.
+        ratio (float, optional): Ratio of the sample size of the treatment group to the reference group. Default is 1.
+        alpha (float, optional): One-sided significance level. Default is 0.05.
+        power (float, optional): Power of the test. Default is 0.80.
+        pooled (bool, optional): Whether or not the pooled method is used. Default is False.
+        continuity_correction (bool, optional): Whether or not the continuity correction is used. Default is False.
+
+    Returns:
+        size(tuple[int, int]): The required sample size.
+    """
+
+    if ratio >= 1:
+
+        def func(reference_size: float) -> float:
+            return (
+                _power(
+                    treatment_proportion,
+                    reference_proportion,
+                    margin,
+                    reference_size * ratio,
+                    reference_size,
+                    alpha,
+                    pooled,
+                    continuity_correction,
+                )
+                - power
+            )
+
+        reference_size = int(ceil(brentq(func, 0.000001, SAMPLE_SIZE_SEARCH_MAX)))
+        treatment_size = int(ceil(reference_size * ratio))
+        return treatment_size, reference_size
+    else:
+
+        def func(treatment_size: float) -> float:
+            return (
+                _power(
+                    treatment_proportion,
+                    reference_proportion,
+                    margin,
+                    treatment_size,
+                    treatment_size * (1 / ratio),
+                    alpha,
+                    pooled,
+                    continuity_correction,
+                )
+                - power
+            )
+
+        treatment_size = ceil(brentq(func, 0.000001, SAMPLE_SIZE_SEARCH_MAX))
+        reference_size = ceil(treatment_size * (1 / ratio))
+        return float(treatment_size), float(reference_size)
+
+
+def solve_treatment_proportion(
+    reference_proportion: float,
+    margin: float,
+    treatment_size: float,
+    reference_size: float,
+    alpha: float = 0.05,
+    power: float = 0.80,
+    pooled: bool = False,
+    continuity_correction: bool = False,
+) -> float:
+    """Estimate the treatment proportion required for the non-inferiority test between two independent proportions.
+
+    Args:
+        reference_proportion (float): Reference proportion.
+        margin (float): Non-inferiority margin, which should be negative when higher is better, otherwise positive.
+        treatment_size (float): Treatment sample size.
+        reference_size (float): Reference sample size.
+        alpha (float, optional): One-sided significance level. Default is 0.05.
+        power (float, optional): Power of the test. Default is 0.80.
+        pooled (bool, optional): Whether or not the pooled method is used. Default is False.
+        continuity_correction (bool, optional): Whether or not the continuity correction is used. Default is False.
+
+    Returns:
+        treatment_proportion(float): The required treatment proportion.
+    """
+
+    def func(treatment_proportion: float) -> float:
+        return (
+            _power(
+                treatment_proportion,
+                reference_proportion,
+                margin,
+                treatment_size,
+                reference_size,
+                alpha,
+                pooled,
+                continuity_correction,
+            )
+            - power
+        )
+
+    treatment_proportion = brentq(func, max(reference_proportion + margin, 0), 1)
+    return treatment_proportion
+
+
+def solve_reference_proportion(
+    treatment_proportion: float,
+    margin: float,
+    treatment_size: float,
+    reference_size: float,
+    alpha: float = 0.05,
+    power: float = 0.80,
+    pooled: bool = False,
+    continuity_correction: bool = False,
+) -> float:
+    """Estimate the reference proportion required for the non-inferiority test between two independent proportions.
+
+    Args:
+        treatment_proportion (float): Treatment proportion.
+        margin (float): Non-inferiority margin, which should be negative when higher is better, otherwise positive.
+        treatment_size (float): Treatment sample size.
+        reference_size (float): Reference sample size.
+        alpha (float, optional): One-sided significance level. Default is 0.05.
+        power (float, optional): Power of the test. Default is 0.80.
+        pooled (bool, optional): Whether or not the pooled method is used. Default is False.
+        continuity_correction (bool, optional): Whether or not the continuity correction is used. Default is False.
+
+    Returns:
+        reference_proportion(float): The required reference proportion.
+    """
+
+    def func(reference_proportion: float) -> float:
+        return (
+            _power(
+                treatment_proportion,
+                reference_proportion,
+                margin,
+                treatment_size,
+                reference_size,
+                alpha,
+                pooled,
+                continuity_correction,
+            )
+            - power
+        )
+
+    reference_proportion = brentq(func, 0, min(treatment_proportion - margin, 1))
+    return reference_proportion
+
+
+def solve_margin(
+    treatment_proportion: float,
+    reference_proportion: float,
+    treatment_size: float,
+    reference_size: float,
+    alpha: float = 0.05,
+    power: float = 0.80,
+    pooled: bool = False,
+    continuity_correction: bool = False,
+) -> float:
+    """Estimate the non-inferiority margin required for the non-inferiority test between two independent proportions.
+
+    Args:
+        treatment_proportion (float): Treatment proportion.
+        reference_proportion (float): Reference proportion.
+        treatment_size (float): Treatment sample size.
+        reference_size (float): Reference sample size.
+        alpha (float, optional): One-sided significance level. Default is 0.05.
+        power (float, optional): Power of the test. Default is 0.80.
+        pooled (bool, optional): Whether or not the pooled method is used. Default is False.
+        continuity_correction (bool, optional): Whether or not the continuity correction is used. Default is False.
+
+    Returns:
+        margin(float): The required non-inferiority margin.
+
+    Notes:
+        The non-inferiority margin should be negative when higher is better, otherwise positive.
+
+        The range of the non-inferiority margin $(\delta)$ is determined by the treatment proportion $(p_1)$ and reference proportion $(p_2)$:
+
+        $$
+        \\begin{cases}
+        (-p_1, \ 0) \cup (p_1-p_2, \ 1-p_2) & , \\text{if } p_1 > p_2 \\\\
+        (-p_2, \ p_1-p_2) \cup (0, \ 1-p_2) & , \\text{if } p_1 < p_2 \\\\
+        (-p_1, \ 0) \cup (0, \ 1-p_2)       & , \\text{if } p_1 = p_2
+        \end{cases}
+        $$
+    """
+
+    def func(margin: float) -> float:
+        return (
+            _power(
+                treatment_proportion,
+                reference_proportion,
+                margin,
+                treatment_size,
+                reference_size,
+                alpha,
+                pooled,
+                continuity_correction,
+            )
+            - power
+        )
+
+    if treatment_proportion > reference_proportion:
+        if func(-treatment_proportion) * func(0) < 0:
+            margin = brentq(func, -treatment_proportion, 0)
+        elif func(treatment_proportion - reference_proportion) * func(1 - reference_proportion) < 0:
+            margin = brentq(func, treatment_proportion - reference_proportion, 1 - reference_proportion)
+    elif treatment_proportion < reference_proportion:
+        if func(-reference_proportion) * func(treatment_proportion - reference_proportion) < 0:
+            margin = brentq(func, -reference_proportion, treatment_proportion - reference_proportion)
+        elif func(0) * func(1 - reference_proportion) < 0:
+            margin = brentq(func, 0, 1 - reference_proportion)
+    elif treatment_proportion == reference_proportion:
+        if func(-treatment_proportion) * func(0) < 0:
+            margin = brentq(func, -treatment_proportion, 0)
+        elif func(0) * func(1 - reference_proportion) < 0:
+            margin = brentq(func, 0, 1 - reference_proportion)
+
+    return float(margin)
 
 
 def _continuity_correction(
