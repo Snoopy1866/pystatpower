6f609bbbaf969140fe0fa8d0f36f1798e65e3f26
Firefly <45487685+Snoopy1866@users.noreply.github.com>
2026-04-21 15:23:09 +0800

    update

--- src/pystatpower/models/proportion/single/difference.py
+++ src/pystatpower/models/proportion/single/difference.py
@@ -1,5 +1,5 @@
 from math import ceil, sqrt
-from typing import Literal
+from typing import Literal, cast
 
 from scipy.optimize import brentq
 from scipy.stats import norm
@@ -7,61 +7,137 @@ from scipy.stats import norm
 from ...._constant import SAMPLE_SIZE_SEARCH_MAX
 
 
-def _power_p0(null_proportion: float, proportion: float, size: int, alpha: float) -> float:
-    power = 1 - norm.cdf(
-        (
-            norm.ppf(1 - alpha / 2) * sqrt(null_proportion * (1 - null_proportion))
-            - sqrt(size) * abs(proportion - null_proportion)
-        )
-        / sqrt(proportion * (1 - proportion))
-    )
+def _power_p0(
+    null_proportion: float, proportion: float, size: float, alternative: Literal["one-sided", "two-sided"], alpha: float
+) -> float:
+    match alternative.lower():
+        case "one-sided":
+            power = 1 - norm.cdf(
+                (
+                    norm.ppf(1 - alpha) * sqrt(null_proportion * (1 - null_proportion))
+                    - sqrt(size) * abs(proportion - null_proportion)
+                )
+                / sqrt(proportion * (1 - proportion))
+            )
+        case "two-sided":
+            power = 2 - (
+                norm.cdf(
+                    (
+                        norm.ppf(1 - alpha / 2) * sqrt(null_proportion * (1 - null_proportion))
+                        - sqrt(size) * (proportion - null_proportion)
+                    )
+                    / sqrt(proportion * (1 - proportion))
+                )
+                + norm.cdf(
+                    (
+                        norm.ppf(1 - alpha / 2) * sqrt(null_proportion * (1 - null_proportion))
+                        + sqrt(size) * (proportion - null_proportion)
+                    )
+                    / sqrt(proportion * (1 - proportion))
+                )
+            )
     return float(power)
 
 
-def _power_p0_cc(null_proportion: float, proportion: float, size: int, alpha: float) -> float:
-    power = 1 - norm.cdf(
-        (
-            norm.ppf(1 - alpha / 2) * sqrt(null_proportion * (1 - null_proportion))
-            - sqrt(size) * (abs(proportion - null_proportion) - 1 / (2 * size))
-        )
-        / sqrt(proportion * (1 - proportion))
-    )
+def _power_p0_cc(
+    null_proportion: float, proportion: float, size: float, alternative: Literal["one-sided", "two-sided"], alpha: float
+) -> float:
+    match alternative.lower():
+        case "one-sided":
+            power = 1 - norm.cdf(
+                (
+                    norm.ppf(1 - alpha) * sqrt(null_proportion * (1 - null_proportion))
+                    - sqrt(size) * (abs(proportion - null_proportion) - 1 / (2 * size))
+                )
+                / sqrt(proportion * (1 - proportion))
+            )
+        case "two-sided":
+            power = 2 - (
+                norm.cdf(
+                    (
+                        norm.ppf(1 - alpha / 2) * sqrt(null_proportion * (1 - null_proportion))
+                        - sqrt(size) * (proportion - null_proportion - 1 / (2 * size))
+                    )
+                    / sqrt(proportion * (1 - proportion))
+                )
+                + norm.cdf(
+                    (
+                        norm.ppf(1 - alpha / 2) * sqrt(null_proportion * (1 - null_proportion))
+                        + sqrt(size) * (proportion - null_proportion + 1 / (2 * size))
+                    )
+                    / sqrt(proportion * (1 - proportion))
+                )
+            )
     return float(power)
 
 
-def _power_phat(null_proportion: float, proportion: float, size: int, alpha: float) -> float:
-    power = 1 - norm.cdf(
-        norm.ppf(1 - alpha / 2) - sqrt(size) * abs(proportion - null_proportion) / sqrt(proportion * (1 - proportion))
-    )
+def _power_phat(
+    null_proportion: float, proportion: float, size: float, alternative: Literal["one-sided", "two-sided"], alpha: float
+) -> float:
+    match alternative:
+        case "one-sided":
+            power = 1 - norm.cdf(
+                norm.ppf(1 - alpha)
+                - sqrt(size) * abs(proportion - null_proportion) / sqrt(proportion * (1 - proportion))
+            )
+        case "two-sided":
+            power = 2 - (
+                norm.cdf(
+                    norm.ppf(1 - alpha / 2)
+                    - sqrt(size) * (proportion - null_proportion) / sqrt(proportion * (1 - proportion))
+                )
+                + norm.cdf(
+                    norm.ppf(1 - alpha / 2)
+                    + sqrt(size) * (proportion - null_proportion) / sqrt(proportion * (1 - proportion))
+                )
+            )
 
     return float(power)
 
 
-def _power_phat_cc(null_proportion: float, proportion: float, size: int, alpha: float) -> float:
-    power = 1 - norm.cdf(
-        norm.ppf(1 - alpha / 2)
-        - sqrt(size) * (abs(proportion - null_proportion) - 1 / (2 * size)) / sqrt(proportion * (1 - proportion))
-    )
+def _power_phat_cc(
+    null_proportion: float, proportion: float, size: float, alternative: Literal["one-sided", "two-sided"], alpha: float
+) -> float:
+    match alternative:
+        case "one-sided":
+            power = 1 - norm.cdf(
+                norm.ppf(1 - alpha)
+                - sqrt(size)
+                * (abs(proportion - null_proportion) - 1 / (2 * size))
+                / sqrt(proportion * (1 - proportion))
+            )
+        case "two-sided":
+            power = 2 - (
+                norm.cdf(
+                    norm.ppf(1 - alpha / 2)
+                    - sqrt(size) * (proportion - null_proportion - 1 / (2 * size)) / sqrt(proportion * (1 - proportion))
+                )
+                + norm.cdf(
+                    norm.ppf(1 - alpha / 2)
+                    + sqrt(size) * (proportion - null_proportion + 1 / (2 * size)) / sqrt(proportion * (1 - proportion))
+                )
+            )
     return float(power)
 
 
 def _power(
     null_proportion: float,
     proportion: float,
-    size: int,
+    size: float,
+    alternative: Literal["one-sided", "two-sided"],
     alpha: float,
-    phat: bool = False,
-    continuity_correction: bool = False,
+    phat: bool,
+    continuity_correction: bool,
 ) -> float:
     match (phat, continuity_correction):
         case (True, True):
-            power = _power_phat_cc(null_proportion, proportion, size, alpha)
+            power = _power_phat_cc(null_proportion, proportion, size, alternative, alpha)
         case (True, False):
-            power = _power_phat(null_proportion, proportion, size, alpha)
+            power = _power_phat(null_proportion, proportion, size, alternative, alpha)
         case (False, True):
-            power = _power_p0_cc(null_proportion, proportion, size, alpha)
+            power = _power_p0_cc(null_proportion, proportion, size, alternative, alpha)
         case (False, False):
-            power = _power_p0(null_proportion, proportion, size, alpha)
+            power = _power_p0(null_proportion, proportion, size, alternative, alpha)
     return power
 
 
@@ -69,6 +145,7 @@ def solve_power(
     null_proportion: float,
     proportion: float,
     size: int,
+    alternative: Literal["one-sided", "two-sided"] = "two-sided",
     alpha: float = 0.05,
     phat: bool = False,
     continuity_correction: bool = False,
@@ -77,9 +154,10 @@ def solve_power(
 
     Args:
         null_proportion (float): Proportion under the null hypothesis.
-        correlation (float): Proportion under the alternative hypothesis.
+        proportion (float): Proportion under the alternative hypothesis.
         size (int): Sample size.
-        alpha (float): Two-sided significance level. Defaults is 0.05.
+        alternative (Literal["one-sided", "two-sided"]): Specify whether the alternative hypothesis of the test is one-sided or two-sided. Default is "two-sided".
+        alpha (float): Significance level. Defaults is 0.05.
         phat (bool, optional): Whether or not to use sample proportion to calculate standard deviation. Defaults is False.
         continuity_correction (bool, optional): Whether or not to use continuity correction. Defaults is False.
 
@@ -87,12 +165,13 @@ def solve_power(
         power(float): Power of the test.
     """
 
-    return _power(null_proportion, proportion, size, alpha, phat, continuity_correction)
+    return _power(null_proportion, proportion, size, alternative, alpha, phat, continuity_correction)
 
 
 def solve_size(
     null_proportion: float,
     proportion: float,
+    alternative: Literal["one-sided", "two-sided"] = "two-sided",
     alpha: float = 0.05,
     power: float = 0.80,
     phat: bool = False,
@@ -102,8 +181,9 @@ def solve_size(
 
     Args:
         null_proportion (float): Proportion under the null hypothesis.
-        correlation (float): Proportion under the alternative hypothesis.
-        alpha (float): Two-sided significance level. Defaults is 0.05.
+        proportion (float): Proportion under the alternative hypothesis.
+        alternative (Literal["one-sided", "two-sided"]): Specify whether the alternative hypothesis of the test is one-sided or two-sided. Default is "two-sided".
+        alpha (float): Significance level. Defaults is 0.05.
         power (float): Power of the test. Defaults is 0.80.
         phat (bool, optional): Whether or not to use sample proportion to calculate standard deviation. Defaults is False
         continuity_correction (bool, optional): Whether or not to use continuity correction. Defaults is False.
@@ -113,27 +193,28 @@ def solve_size(
     """
 
     def func(size: float) -> float:
-        return _power(null_proportion, proportion, size, alpha, phat, continuity_correction) - power
+        return _power(null_proportion, proportion, size, alternative, alpha, phat, continuity_correction) - power
 
-    size = brentq(func, 0.000001, SAMPLE_SIZE_SEARCH_MAX)
-    return ceil(size)
+    return ceil(brentq(func, 0.000001, SAMPLE_SIZE_SEARCH_MAX))
 
 
 def solve_null_proportion(
     proportion: float,
     size: int,
+    alternative: Literal["one-sided", "two-sided"] = "two-sided",
     alpha: float = 0.05,
     power: float = 0.80,
     phat: bool = False,
     continuity_correction: bool = False,
     proportion_selection: Literal["lower", "upper"] = "lower",
-):
+) -> float:
     """Estimate the null proportion required for the difference test between one proportion and a null proportion.
 
     Args:
         proportion (float): Proportion under the alternative hypothesis.
         size (int): Sample size.
-        alpha (float): Two-sided significance level. Defaults is 0.05.
+        alternative (Literal["one-sided", "two-sided"]): Specify whether the alternative hypothesis of the test is one-sided or two-sided. Default is "two-sided".
+        alpha (float): Significance level. Defaults is 0.05.
         power (float): Power of the test. Defaults is 0.80.
         phat (bool, optional): Whether or not to use sample proportion to calculate standard deviation. Defaults is False.
         continuity_correction (bool, optional): Whether or not to use continuity correction. Defaults is False.
@@ -150,16 +231,16 @@ def solve_null_proportion(
     """
 
     def func(null_proportion: float) -> float:
-        return _power(null_proportion, proportion, size, alpha, phat, continuity_correction) - power
+        return _power(null_proportion, proportion, size, alternative, alpha, phat, continuity_correction) - power
 
     null_proportions: list[float] = []
     lower_bound, upper_bound = 0.000001, proportion
     if func(lower_bound) * func(upper_bound) < 0:
-        null_proportions.append(float(brentq(func, lower_bound, upper_bound)))
+        null_proportions.append(brentq(func, lower_bound, upper_bound))
 
     lower_bound, upper_bound = proportion, 0.999999
     if func(lower_bound) * func(upper_bound) < 0:
-        null_proportions.append(float(brentq(func, lower_bound, upper_bound)))
+        null_proportions.append(brentq(func, lower_bound, upper_bound))
 
     if len(null_proportions) == 2:
         match proportion_selection.lower():
@@ -174,6 +255,7 @@ def solve_null_proportion(
 def solve_proportion(
     null_proportion: float,
     size: int,
+    alternative: Literal["one-sided", "two-sided"] = "two-sided",
     alpha: float = 0.05,
     power: float = 0.80,
     phat: bool = False,
@@ -185,7 +267,8 @@ def solve_proportion(
     Args:
         null_proportion (float): Proportion under the null hypothesis.
         size (int): Sample size.
-        alpha (float): Two-sided significance level. Defaults is 0.05.
+        alternative (Literal["one-sided", "two-sided"]): Specify whether the alternative hypothesis of the test is one-sided or two-sided. Default is "two-sided".
+        alpha (float): Significance level. Defaults is 0.05.
         power (float): Power of the test. Defaults is 0.80.
         phat (bool, optional): Whether or not to use sample proportion to calculate standard deviation. Defaults is False.
         continuity_correction (bool, optional): Whether or not to use continuity correction. Defaults is False.
@@ -202,7 +285,7 @@ def solve_proportion(
     """
 
     def func(proportion: float) -> float:
-        return _power(null_proportion, proportion, size, alpha, phat, continuity_correction) - power
+        return _power(null_proportion, proportion, size, alternative, alpha, phat, continuity_correction) - power
 
     proportions: list[float] = []
     lower_bound, upper_bound = 0.000001, null_proportion
