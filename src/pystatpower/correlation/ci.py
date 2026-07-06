e34230e95d6e2f6083940dadfc067a8b741aedc7
Firefly <45487685+Snoopy1866@users.noreply.github.com>
2026-07-06 14:55:40 +0800

    refactor(correlation.ci): optimize the algorithm and docstring

--- src/pystatpower/correlation/ci.py
+++ src/pystatpower/correlation/ci.py
@@ -13,23 +13,24 @@ def _distance_not_adjusted(
     conf_level: float,
     interval_type: Literal["two-sided", "lower", "upper"],
 ) -> float:
-    """Calculate the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound"""
+    """Calculate the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound."""
 
     alpha = 1 - conf_level
     zr = atanh(correlation)
+    se_recip = sqrt(size - 3)
 
     match interval_type:
         case "two-sided":
-            L = zr - norm.ppf(1 - alpha / 2) / sqrt(size - 3)
-            U = zr + norm.ppf(1 - alpha / 2) / sqrt(size - 3)
+            L = zr - norm.ppf(1 - alpha / 2) / se_recip
+            U = zr + norm.ppf(1 - alpha / 2) / se_recip
             distance = tanh(U) - tanh(L)
         case "lower":
-            L = zr - norm.ppf(1 - alpha) / sqrt(size - 3)
+            L = zr - norm.ppf(1 - alpha) / se_recip
             # U = 1
             distance = correlation - tanh(L)
         case "upper":
             # L = -1
-            U = zr + norm.ppf(1 - alpha) / sqrt(size - 3)
+            U = zr + norm.ppf(1 - alpha) / se_recip
             distance = tanh(U) - correlation
 
     return float(distance)
@@ -41,24 +42,25 @@ def _distance_adjusted(
     conf_level: float,
     interval_type: Literal["two-sided", "lower", "upper"],
 ) -> float:
-    """Calculate the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound, adjusted for bias."""
+    """Calculate the width of correlation coefficient confidence interval or the distance from the correlation coefficient to the confidence bound, adjusted for bias."""
 
     alpha = 1 - conf_level
     zr = atanh(correlation)
     bias = 0.5 * correlation / (size - 1)
+    se_recip = sqrt(size - 3)
 
     match interval_type:
         case "two-sided":
-            L = zr - bias - norm.ppf(1 - alpha / 2) / sqrt(size - 3)
-            U = zr - bias + norm.ppf(1 - alpha / 2) / sqrt(size - 3)
+            L = zr - bias - norm.ppf(1 - alpha / 2) / se_recip
+            U = zr - bias + norm.ppf(1 - alpha / 2) / se_recip
             distance = tanh(U) - tanh(L)
         case "lower":
-            L = zr - bias - norm.ppf(1 - alpha) / sqrt(size - 3)
+            L = zr - bias - norm.ppf(1 - alpha) / se_recip
             # U = 1
             distance = correlation - tanh(L)
         case "upper":
             # L = -1
-            U = zr - bias + norm.ppf(1 - alpha) / sqrt(size - 3)
+            U = zr - bias + norm.ppf(1 - alpha) / se_recip
             distance = tanh(U) - correlation
 
     return float(distance)
@@ -71,7 +73,7 @@ def _distance(
     interval_type: Literal["two-sided", "lower", "upper"],
     bias_adj: bool,
 ) -> float:
-    """Calculate the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound"""
+    """Calculate the width of correlation coefficient confidence interval or the distance from the correlation coefficient to the confidence bound."""
 
     if bias_adj:
         return _distance_adjusted(correlation, size, conf_level, interval_type)
@@ -87,29 +89,30 @@ def solve_distance(
     interval_type: Literal["two-sided", "lower", "upper"] = "two-sided",
     bias_adj: bool = False,
 ) -> float:
-    """Calculate the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound
+    """Calculate the width of correlation coefficient confidence interval or the distance from the correlation coefficient to the confidence bound.
 
     Args:
-        correlation (float):
-            Actual correlation coefficient ($r$). Must be between -1 and 1.
-        size (int):
-            Sample size $n$.
-        conf_level (float, optional):
+        correlation:
+            Correlation coefficient.
+        size:
+            Sample size.
+        conf_level:
             Condidence level.
-        interval_type (Literal["two-sided", "lower", "upper"], optional):
+        interval_type:
             Type of the confidence interval.
 
             - `'two-sided'`: Two-sided confidence interval.
             - `'lower'`: Lower one-sided confidence interval.
             - `'upper'`: Upper one-sided confidence interval.
-        bias_adj (bool, optional):
-            Whether to adjust for bias.
+        bias_adj:
+            Whether to adjust for the bias.
 
     Returns:
-        (float): The correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound.
+        - If `interval_type` is `'two-sided'`, returns the width of correlation coefficient confidence interval
+        - If `interval_type` is `'greater'` or `'less'`, returns the distance from the correlation coefficient to the confidence bound.
     """
 
-    return float(_distance(correlation, size, conf_level, interval_type, bias_adj))
+    return _distance(correlation, size, conf_level, interval_type, bias_adj)
 
 
 def solve_size(
@@ -120,20 +123,20 @@ def solve_size(
     interval_type: Literal["two-sided", "lower", "upper"] = "two-sided",
     bias_adj: bool = False,
 ) -> int:
-    """Estimate the required sample size, given the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound
+    """Estimate the required sample size.
 
     Args:
-        correlation (float):
-            Actual correlation coefficient ($r$). Must be between -1 and 1.
-        distance (float):
-            Correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound.
+        correlation:
+            Correlation coefficient.
+        distance:
+            The width of correlation coefficient confidence interval or the distance from the correlation coefficient to the confidence bound.
 
-            - If `interval_type` = `'two-sided'`, specify the correlation coefficient two-sided confidence interval width.
-            - If `interval_type` = `'lower'`, specify the distance from the correlation coefficient to the lower one-sided confidence bound.
-            - If `interval_type` = `'upper'`, specify the distance from the correlation coefficient to the upper one-sided confidence bound.
-        conf_level (float, optional):
+            - If `interval_type` = `'two-sided'`, the width of correlation coefficient two-sided confidence interval id required.
+            - If `interval_type` = `'lower'`, the distance from the correlation coefficient to the lower one-sided confidence bound is required.
+            - If `interval_type` = `'upper'`, the distance from the correlation coefficient to the upper one-sided confidence bound is required.
+        conf_level:
             Condidence level.
-        interval_type (Literal["two-sided", "lower", "upper"], optional):
+        interval_type:
             Type of the confidence interval.
 
             - `'two-sided'`: Two-sided confidence interval.
@@ -143,7 +146,7 @@ def solve_size(
             Whether to adjust for bias.
 
     Returns:
-        (float): The required sample size $n$.
+        The required sample size.
     """
 
     def func(size: float) -> float:
@@ -166,50 +169,34 @@ def solve_correlation(
     conf_level: float = 0.95,
     interval_type: Literal["two-sided", "lower", "upper"] = "two-sided",
     bias_adj: bool = False,
-) -> float | tuple[float, float]:
-    """Estimate the required correlation coefficient, given the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound
+) -> float:
+    """Estimate the required correlation coefficient.
 
     Args:
-        distance (float):
-            Correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound.
+        distance:
+            The width of correlation coefficient confidence interval or the distance from the correlation coefficient to the confidence bound.
 
-            - If `interval_type` = `'two-sided'`, specify the correlation coefficient two-sided confidence interval width.
-            - If `interval_type` = `'lower'`, specify the distance from the correlation coefficient to the lower one-sided confidence bound.
-            - If `interval_type` = `'upper'`, specify the distance from the correlation coefficient to the upper one-sided confidence bound.
-        size (int):
-            Sample size $n$.
-        conf_level (float, optional):
+            - If `interval_type` = `'two-sided'`, the width of correlation coefficient two-sided confidence interval is required.
+            - If `interval_type` = `'lower'`, the distance from the correlation coefficient to the lower one-sided confidence bound is required.
+            - If `interval_type` = `'upper'`, the distance from the correlation coefficient to the upper one-sided confidence bound is required.
+        size:
+            Sample size.
+        conf_level:
             Condidence level.
-        interval_type (Literal["two-sided", "lower", "upper"], optional):
+        interval_type:
             Type of the confidence interval.
 
             - `'two-sided'`: Two-sided confidence interval.
             - `'lower'`: Lower one-sided confidence interval.
             - `'upper'`: Upper one-sided confidence interval.
-        bias_adj (bool, optional):
-            Whether to adjust for bias.
+        bias_adj:
+            Whether to adjust for the bias.
 
     Returns:
-        (float | tuple[float, float]):
-            The required correlation coefficient $r$, or a tuple of correlation coefficient if two solutions found.
-            Note that this function only returns positive correlation coefficient.
+        The required correlation coefficient.
     """
 
     def func(correlation: float) -> float:
         return _distance(correlation, size, conf_level, interval_type, bias_adj) - distance
 
-    lower_bound = 1e-12
-    upper_bound = 1 - 1e-12
-
-    if func(lower_bound) * func(upper_bound) < 0:
-        return float(brentq(func, lower_bound, upper_bound))
-    else:
-        optimize_res: OptimizeResult = minimize_scalar(
-            lambda correlation: -func(correlation), bounds=(lower_bound, upper_bound)
-        )
-        if optimize_res.success:
-            x = float(optimize_res.x)
-            if func(x) < 0:
-                raise SolutionNotFoundError("Solution not found")
-            else:
-                return float(brentq(func, lower_bound, x)), float(brentq(func, x, upper_bound))
+    return float(brentq(func, 0, 1 - 1e-12))
