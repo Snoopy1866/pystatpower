f163dee9c956dad2d4cb4b9043fce427d338befd
wtwang <45487685+Snoopy1866@users.noreply.github.com>
2026-07-06 15:36:06 +0800

    refactor(correlation.ci): optimize the algorithm and docstring, remove `solve_correlation` (#495)
    When the sample size and significance level are fixed, the confidence interval width of the correlation coefficient is not monotonic within [0, 1], which makes `solve_correlation` less practical, so it is removed.

--- src/pystatpower/correlation/ci.py
+++ src/pystatpower/correlation/ci.py
@@ -1,10 +1,8 @@
 from math import atanh, ceil, sqrt, tanh
 from typing import Literal
 
-from scipy.optimize import OptimizeResult, brentq, minimize_scalar
+from scipy.optimize import brentq
 from scipy.stats import norm
-
-from ..exceptions import SolutionNotFoundError
 
 
 def _distance_not_adjusted(
@@ -13,23 +11,24 @@ def _distance_not_adjusted(
     conf_level: float,
     interval_type: Literal["two-sided", "lower", "upper"],
 ) -> float:
-    """Calculate the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound"""
+    """Calculate the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound."""
 
     alpha = 1 - conf_level
     zr = atanh(correlation)
+    se_recip = sqrt(size - 3)
 
     match interval_type:
         case "two-sided":
-            L = zr - norm.ppf(1 - alpha / 2) / sqrt(size - 3)
-            U = zr + norm.ppf(1 - alpha / 2) / sqrt(size - 3)
+            L = zr - norm.ppf(1 - alpha / 2) / se_recip
+            U = zr + norm.ppf(1 - alpha / 2) / se_recip
             distance = tanh(U) - tanh(L)
         case "lower":
-            L = zr - norm.ppf(1 - alpha) / sqrt(size - 3)
+            L = zr - norm.ppf(1 - alpha) / se_recip
             # U = 1
             distance = correlation - tanh(L)
         case "upper":
             # L = -1
-            U = zr + norm.ppf(1 - alpha) / sqrt(size - 3)
+            U = zr + norm.ppf(1 - alpha) / se_recip
             distance = tanh(U) - correlation
 
     return float(distance)
@@ -41,24 +40,25 @@ def _distance_adjusted(
     conf_level: float,
     interval_type: Literal["two-sided", "lower", "upper"],
 ) -> float:
-    """Calculate the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound, adjusted for bias."""
+    """Calculate the width of correlation coefficient confidence interval or the distance from the correlation coefficient to the confidence bound, adjusted for bias."""
 
     alpha = 1 - conf_level
     zr = atanh(correlation)
     bias = 0.5 * correlation / (size - 1)
+    se_recip = sqrt(size - 3)
 
     match interval_type:
         case "two-sided":
-            L = zr - bias - norm.ppf(1 - alpha / 2) / sqrt(size - 3)
-            U = zr - bias + norm.ppf(1 - alpha / 2) / sqrt(size - 3)
+            L = zr - bias - norm.ppf(1 - alpha / 2) / se_recip
+            U = zr - bias + norm.ppf(1 - alpha / 2) / se_recip
             distance = tanh(U) - tanh(L)
         case "lower":
-            L = zr - bias - norm.ppf(1 - alpha) / sqrt(size - 3)
+            L = zr - bias - norm.ppf(1 - alpha) / se_recip
             # U = 1
             distance = correlation - tanh(L)
         case "upper":
             # L = -1
-            U = zr - bias + norm.ppf(1 - alpha) / sqrt(size - 3)
+            U = zr - bias + norm.ppf(1 - alpha) / se_recip
             distance = tanh(U) - correlation
 
     return float(distance)
@@ -71,7 +71,7 @@ def _distance(
     interval_type: Literal["two-sided", "lower", "upper"],
     bias_adj: bool,
 ) -> float:
-    """Calculate the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound"""
+    """Calculate the width of correlation coefficient confidence interval or the distance from the correlation coefficient to the confidence bound."""
 
     if bias_adj:
         return _distance_adjusted(correlation, size, conf_level, interval_type)
@@ -87,29 +87,30 @@ def solve_distance(
     interval_type: Literal["two-sided", "lower", "upper"] = "two-sided",
     bias_adj: bool = False,
 ) -> float:
-    """Calculate the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound
+    """Calculate the width of correlation coefficient confidence interval or the distance from the correlation coefficient to the confidence bound.
 
     Args:
-        correlation (float):
-            Actual correlation coefficient ($r$). Must be between -1 and 1.
-        size (int):
-            Sample size $n$.
-        conf_level (float, optional):
-            Condidence level.
-        interval_type (Literal["two-sided", "lower", "upper"], optional):
+        correlation:
+            Correlation coefficient.
+        size:
+            Sample size.
+        conf_level:
+            Confidence level.
+        interval_type:
             Type of the confidence interval.
 
             - `'two-sided'`: Two-sided confidence interval.
             - `'lower'`: Lower one-sided confidence interval.
             - `'upper'`: Upper one-sided confidence interval.
-        bias_adj (bool, optional):
-            Whether to adjust for bias.
+        bias_adj:
+            Whether to adjust for the bias.
 
     Returns:
-        (float): The correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound.
+        - If `interval_type` is `'two-sided'`, returns the width of correlation coefficient confidence interval
+        - If `interval_type` is `'lower'` or `'upper'`, returns the distance from the correlation coefficient to the confidence bound.
     """
 
-    return float(_distance(correlation, size, conf_level, interval_type, bias_adj))
+    return _distance(correlation, size, conf_level, interval_type, bias_adj)
 
 
 def solve_size(
@@ -120,20 +121,20 @@ def solve_size(
     interval_type: Literal["two-sided", "lower", "upper"] = "two-sided",
     bias_adj: bool = False,
 ) -> int:
-    """Estimate the required sample size, given the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound
+    """Estimate the required sample size.
 
     Args:
-        correlation (float):
-            Actual correlation coefficient ($r$). Must be between -1 and 1.
-        distance (float):
-            Correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound.
+        correlation:
+            Correlation coefficient.
+        distance:
+            The width of correlation coefficient confidence interval or the distance from the correlation coefficient to the confidence bound.
 
-            - If `interval_type` = `'two-sided'`, specify the correlation coefficient two-sided confidence interval width.
-            - If `interval_type` = `'lower'`, specify the distance from the correlation coefficient to the lower one-sided confidence bound.
-            - If `interval_type` = `'upper'`, specify the distance from the correlation coefficient to the upper one-sided confidence bound.
-        conf_level (float, optional):
-            Condidence level.
-        interval_type (Literal["two-sided", "lower", "upper"], optional):
+            - If `interval_type` = `'two-sided'`, the width of correlation coefficient two-sided confidence interval id required.
+            - If `interval_type` = `'lower'`, the distance from the correlation coefficient to the lower one-sided confidence bound is required.
+            - If `interval_type` = `'upper'`, the distance from the correlation coefficient to the upper one-sided confidence bound is required.
+        conf_level:
+            Confidence level.
+        interval_type:
             Type of the confidence interval.
 
             - `'two-sided'`: Two-sided confidence interval.
@@ -143,7 +144,7 @@ def solve_size(
             Whether to adjust for bias.
 
     Returns:
-        (float): The required sample size $n$.
+        The required sample size.
     """
 
     def func(size: float) -> float:
@@ -157,59 +158,3 @@ def solve_size(
         return ceil(lower_bound)
     else:
         return ceil(brentq(func, lower_bound, upper_bound))
-
-
-def solve_correlation(
-    *,
-    distance: float,
-    size: int,
-    conf_level: float = 0.95,
-    interval_type: Literal["two-sided", "lower", "upper"] = "two-sided",
-    bias_adj: bool = False,
-) -> float | tuple[float, float]:
-    """Estimate the required correlation coefficient, given the correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound
-
-    Args:
-        distance (float):
-            Correlation coefficient confidence interval width or the distance from the correlation coefficient to the confidence bound.
-
-            - If `interval_type` = `'two-sided'`, specify the correlation coefficient two-sided confidence interval width.
-            - If `interval_type` = `'lower'`, specify the distance from the correlation coefficient to the lower one-sided confidence bound.
-            - If `interval_type` = `'upper'`, specify the distance from the correlation coefficient to the upper one-sided confidence bound.
-        size (int):
-            Sample size $n$.
-        conf_level (float, optional):
-            Condidence level.
-        interval_type (Literal["two-sided", "lower", "upper"], optional):
-            Type of the confidence interval.
-
-            - `'two-sided'`: Two-sided confidence interval.
-            - `'lower'`: Lower one-sided confidence interval.
-            - `'upper'`: Upper one-sided confidence interval.
-        bias_adj (bool, optional):
-            Whether to adjust for bias.
-
-    Returns:
-        (float | tuple[float, float]):
-            The required correlation coefficient $r$, or a tuple of correlation coefficient if two solutions found.
-            Note that this function only returns positive correlation coefficient.
-    """
-
-    def func(correlation: float) -> float:
-        return _distance(correlation, size, conf_level, interval_type, bias_adj) - distance
-
-    lower_bound = 1e-12
-    upper_bound = 1 - 1e-12
-
-    if func(lower_bound) * func(upper_bound) < 0:
-        return float(brentq(func, lower_bound, upper_bound))
-    else:
-        optimize_res: OptimizeResult = minimize_scalar(
-            lambda correlation: -func(correlation), bounds=(lower_bound, upper_bound)
-        )
-        if optimize_res.success:
-            x = float(optimize_res.x)
-            if func(x) < 0:
-                raise SolutionNotFoundError("Solution not found")
-            else:
-                return float(brentq(func, lower_bound, x)), float(brentq(func, x, upper_bound))
--- tests/test_correlation/test_ci.py
+++ tests/test_correlation/test_ci.py
@@ -3,13 +3,12 @@ from typing import Literal
 
 import pytest
 
-from pystatpower.correlation.ci import solve_distance, solve_size, solve_correlation
-from pystatpower.exceptions import SolutionNotFoundError
+from pystatpower.correlation.ci import solve_distance, solve_size
 
 from tests.models import BaseTestCase
 
 
-@dataclass
+@dataclass(kw_only=True)
 class TestCase(BaseTestCase):
     correlation: float
     size: int
@@ -22,78 +21,78 @@ class TestCase(BaseTestCase):
 
 case_group_not_bias_adj = (
     [
-        # correlation = 0.05 to 0.95 by 0.05, conf_level = 0.95, alternative = "two-sided", bias_adj = False
-        TestCase(correlation=correlation, size=size, conf_level=0.95, interval_type="two-sided", bias_adj=False, distance=distance, actual_distance=actual_distance)
-        for correlation, size, distance, actual_distance in [
-            (0.05, 1530, 0.10, 0.099979332),
-            (0.10, 1507, 0.10, 0.099984091),
-            (0.15, 1469, 0.10, 0.099994088),
-            (0.20, 1417, 0.10, 0.099995093),
-            (0.25, 1352, 0.10, 0.099978952),
-            (0.30, 1274, 0.10, 0.099983282),
-            (0.35, 1185, 0.10, 0.099981330),
-            (0.40, 1086, 0.10, 0.099994484),
-            (0.45, 980, 0.10, 0.099962668),
-            (0.50, 867, 0.10, 0.099981828),
-            (0.55, 751, 0.10, 0.099954382),
-            (0.60, 633, 0.10, 0.099967040),
-            (0.65, 517, 0.10, 0.099916027),
-            (0.70, 404, 0.10, 0.099982215),
-            (0.75, 299, 0.10, 0.099975088),
-            (0.80, 205, 0.10, 0.099865422),
-            (0.85, 125, 0.10, 0.099683160),
-            (0.90, 62, 0.10, 0.099963173),
-            (0.95, 22, 0.10, 0.097922665),
+        # correlation = 0.05 to 0.95 by 0.05, conf_level = 0.95, alternative = "two-sided", distance = 0.10, bias_adj = False
+        TestCase(correlation=correlation, size=size, conf_level=0.95, interval_type="two-sided", bias_adj=False, distance=0.10, actual_distance=actual_distance)
+        for correlation, size, actual_distance in [
+            (0.05, 1530, 0.099979331717472),
+            (0.10, 1507, 0.099984091436488),
+            (0.15, 1469, 0.099994087978655),
+            (0.20, 1417, 0.099995092984861),
+            (0.25, 1352, 0.099978951865763),
+            (0.30, 1274, 0.099983282146451),
+            (0.35, 1185, 0.099981330392455),
+            (0.40, 1086, 0.099994483830285),
+            (0.45, 980, 0.099962668234808),
+            (0.50, 867, 0.099981828376062),
+            (0.55, 751, 0.099954381700265),
+            (0.60, 633, 0.099967040247887),
+            (0.65, 517, 0.099916027104486),
+            (0.70, 404, 0.099982215320462),
+            (0.75, 299, 0.099975087516713),
+            (0.80, 205, 0.099865422390227),
+            (0.85, 125, 0.099683159562744),
+            (0.90, 62, 0.099963172662623),
+            (0.95, 22, 0.097922665356393),
         ]
     ]
     + [
-        # correlation = 0.05 to 0.95 by 0.05, conf_level = 0.95, alternative = "upper", bias_adj = False
-        TestCase(correlation=correlation, size=size, conf_level=0.95, interval_type="upper", bias_adj=False, distance=distance, actual_distance=actual_distance)
-        for correlation, size, distance, actual_distance in [
-            (0.05, 268, 0.10, 0.099945111),
-            (0.10, 262, 0.10, 0.099816657),
-            (0.15, 252, 0.10, 0.099968113),
-            (0.20, 241, 0.10, 0.099847999),
-            (0.25, 227, 0.10, 0.099886326),
-            (0.30, 211, 0.10, 0.099933430),
-            (0.35, 194, 0.10, 0.099809303),
-            (0.40, 175, 0.10, 0.099821253),
-            (0.45, 155, 0.10, 0.099814585),
-            (0.50, 134, 0.10, 0.099917109),
-            (0.55, 113, 0.10, 0.099949897),
-            (0.60, 93, 0.10, 0.099606807),
-            (0.65, 73, 0.10, 0.099536426),
-            (0.70, 54, 0.10, 0.099644694),
-            (0.75, 37, 0.10, 0.099692333),
-            (0.80, 23, 0.10, 0.098890934),
-            (0.85, 12, 0.10, 0.097263613),
-            (0.90, 4, 0.10, 0.096085072),
-            (0.95, 4, 0.10, 0.048090811),
+        # correlation = 0.05 to 0.95 by 0.05, conf_level = 0.95, alternative = "upper", distance = 0.10, bias_adj = False
+        TestCase(correlation=correlation, size=size, conf_level=0.95, interval_type="upper", bias_adj=False, distance=0.10, actual_distance=actual_distance)
+        for correlation, size, actual_distance in [
+            (0.05, 268, 0.099945111385950),
+            (0.10, 262, 0.099816657006419),
+            (0.15, 252, 0.099968112777152),
+            (0.20, 241, 0.099847999108691),
+            (0.25, 227, 0.099886326471037),
+            (0.30, 211, 0.099933430170927),
+            (0.35, 194, 0.099809302872852),
+            (0.40, 175, 0.099821253045578),
+            (0.45, 155, 0.099814585407058),
+            (0.50, 134, 0.099917108711245),
+            (0.55, 113, 0.099949897421270),
+            (0.60, 93, 0.099606806897013),
+            (0.65, 73, 0.099536425872647),
+            (0.70, 54, 0.099644693746844),
+            (0.75, 37, 0.099692332674108),
+            (0.80, 23, 0.098890934167862),
+            (0.85, 12, 0.097263612520933),
+            (0.90, 4, 0.096085072368805),
+            (0.95, 4, 0.048090811067751),
         ]
     ]
     + [
-        # correlation = 0.05 to 0.95 by 0.05, conf_level = 0.95, alternative = "lower", bias_adj = False
-        TestCase(correlation=correlation, size=size, conf_level=0.95, interval_type="lower", bias_adj=False, distance=distance, actual_distance=actual_distance)
-        for correlation, size, distance, actual_distance in [
-            (0.05, 274, 0.10, 0.099834696),
-            (0.10, 272, 0.10, 0.099953148),
-            (0.15, 268, 0.10, 0.099943989),
-            (0.20, 262, 0.10, 0.099810938),
-            (0.25, 252, 0.10, 0.099966753),
-            (0.30, 241, 0.10, 0.099839661),
-            (0.35, 227, 0.10, 0.099878563),
-            (0.40, 211, 0.10, 0.099927887),
-            (0.45, 194, 0.10, 0.099790214),
-            (0.50, 175, 0.10, 0.099799846),
-            (0.55, 155, 0.10, 0.099788059),
-            (0.60, 134, 0.10, 0.099902876),
-            (0.65, 113, 0.10, 0.099939493),
-            (0.70, 93, 0.10, 0.099507075),
-            (0.75, 73, 0.10, 0.099388989),
-            (0.80, 54, 0.10, 0.099497394),
-            (0.85, 37, 0.10, 0.099515798),
-            (0.90, 23, 0.10, 0.097919260),
-            (0.95, 12, 0.10, 0.092586343),
+        # correlation = 0.05 to 0.95 by 0.05, conf_level = 0.95, alternative = "lower", distance = 0.10, bias_adj = False
+        TestCase(correlation=correlation, size=size, conf_level=0.95, interval_type="lower", bias_adj=False, distance=0.10, actual_distance=actual_distance)
+        for correlation, size, actual_distance in [
+            (0.05, 274, 0.099834696441772),
+            (0.10, 272, 0.099953147908188),
+            (0.15, 268, 0.099943988974280),
+            (0.20, 262, 0.099810938370096),
+            (0.25, 252, 0.099966752707981),
+            (0.30, 241, 0.099839660802411),
+            (0.35, 227, 0.099878563360771),
+            (0.40, 211, 0.099927886685647),
+            (0.45, 194, 0.099790213511564),
+            (0.50, 175, 0.099799846343275),
+            (0.55, 155, 0.099788059009436),
+            (0.60, 134, 0.099902875608244),
+            (0.65, 113, 0.099939492795180),
+            (0.70, 93, 0.099507075241349),
+            (0.75, 73, 0.099388988549015),
+            (0.80, 54, 0.099497393554749),
+            (0.85, 37, 0.099515797845095),
+            (0.90, 23, 0.097919259752027),
+            (0.95, 12, 0.092586342812536),
         ]
     ]
 )
@@ -101,78 +100,78 @@ case_group_not_bias_adj = (
 
 case_group_bias_adj = (
     [
-        # correlation = 0.05 to 0.95 by 0.05, conf_level = 0.95, alternative = "two-sided", bias_adj = True
-        TestCase(correlation=correlation, size=size, conf_level=0.95, interval_type="two-sided", bias_adj=True, distance=distance, actual_distance=actual_distance)
-        for correlation, size, distance, actual_distance in [
-            (0.05, 1530, 0.10, 0.099979332),
-            (0.10, 1507, 0.10, 0.099984091),
-            (0.15, 1469, 0.10, 0.099994088),
-            (0.20, 1417, 0.10, 0.099995093),
-            (0.25, 1352, 0.10, 0.099978952),
-            (0.30, 1274, 0.10, 0.099983282),
-            (0.35, 1185, 0.10, 0.099981330),
-            (0.40, 1086, 0.10, 0.099994484),
-            (0.45, 980, 0.10, 0.099962668),
-            (0.50, 867, 0.10, 0.099981828),
-            (0.55, 751, 0.10, 0.099954382),
-            (0.60, 633, 0.10, 0.099967040),
-            (0.65, 517, 0.10, 0.099916027),
-            (0.70, 404, 0.10, 0.099982215),
-            (0.75, 299, 0.10, 0.099975088),
-            (0.80, 205, 0.10, 0.099865422),
-            (0.85, 125, 0.10, 0.099683160),
-            (0.90, 62, 0.10, 0.099963173),
-            (0.95, 22, 0.10, 0.097922665),
+        # correlation = 0.05 to 0.95 by 0.05, conf_level = 0.95, alternative = "two-sided", distance = 0.10, bias_adj = True
+        TestCase(correlation=correlation, size=size, conf_level=0.95, interval_type="two-sided", bias_adj=True, distance=0.10, actual_distance=actual_distance)
+        for correlation, size, actual_distance in [
+            (0.05, 1530, 0.099979332),
+            (0.10, 1507, 0.099984091),
+            (0.15, 1469, 0.099994088),
+            (0.20, 1417, 0.099995093),
+            (0.25, 1352, 0.099978952),
+            (0.30, 1274, 0.099983282),
+            (0.35, 1185, 0.099981330),
+            (0.40, 1086, 0.099994484),
+            (0.45, 980, 0.099962668),
+            (0.50, 867, 0.099981828),
+            (0.55, 751, 0.099954382),
+            (0.60, 633, 0.099967040),
+            (0.65, 517, 0.099916027),
+            (0.70, 404, 0.099982215),
+            (0.75, 299, 0.099975088),
+            (0.80, 205, 0.099865422),
+            (0.85, 125, 0.099683160),
+            (0.90, 62, 0.099963173),
+            (0.95, 22, 0.097922665),
         ]
     ]
     + [
-        # correlation = 0.05 to 0.95 by 0.05, conf_level = 0.95, alternative = "upper", bias_adj = True
-        TestCase(correlation=correlation, size=size, conf_level=0.95, interval_type="upper", bias_adj=True, distance=distance, actual_distance=actual_distance)
-        for correlation, size, distance, actual_distance in [
-            (0.05, 268, 0.10, 0.099945111),
-            (0.10, 262, 0.10, 0.099816657),
-            (0.15, 252, 0.10, 0.099968113),
-            (0.20, 241, 0.10, 0.099847999),
-            (0.25, 227, 0.10, 0.099886326),
-            (0.30, 211, 0.10, 0.099933430),
-            (0.35, 194, 0.10, 0.099809303),
-            (0.40, 175, 0.10, 0.099821253),
-            (0.45, 155, 0.10, 0.099814585),
-            (0.50, 134, 0.10, 0.099917109),
-            (0.55, 113, 0.10, 0.099949897),
-            (0.60, 93, 0.10, 0.099606807),
-            (0.65, 73, 0.10, 0.099536426),
-            (0.70, 54, 0.10, 0.099644694),
-            (0.75, 37, 0.10, 0.099692333),
-            (0.80, 23, 0.10, 0.098890934),
-            (0.85, 12, 0.10, 0.097263613),
-            (0.90, 4, 0.10, 0.096085072),
-            (0.95, 4, 0.10, 0.048090811),
+        # correlation = 0.05 to 0.95 by 0.05, conf_level = 0.95, alternative = "upper", distance = 0.10, bias_adj = True
+        TestCase(correlation=correlation, size=size, conf_level=0.95, interval_type="upper", bias_adj=True, distance=0.10, actual_distance=actual_distance)
+        for correlation, size, actual_distance in [
+            (0.05, 268, 0.099945111),
+            (0.10, 262, 0.099816657),
+            (0.15, 252, 0.099968113),
+            (0.20, 241, 0.099847999),
+            (0.25, 227, 0.099886326),
+            (0.30, 211, 0.099933430),
+            (0.35, 194, 0.099809303),
+            (0.40, 175, 0.099821253),
+            (0.45, 155, 0.099814585),
+            (0.50, 134, 0.099917109),
+            (0.55, 113, 0.099949897),
+            (0.60, 93, 0.099606807),
+            (0.65, 73, 0.099536426),
+            (0.70, 54, 0.099644694),
+            (0.75, 37, 0.099692333),
+            (0.80, 23, 0.098890934),
+            (0.85, 12, 0.097263613),
+            (0.90, 4, 0.096085072),
+            (0.95, 4, 0.048090811),
         ]
     ]
     + [
-        # correlation = 0.05 to 0.95 by 0.05, conf_level = 0.95, alternative = "lower", bias_adj = True
-        TestCase(correlation=correlation, size=size, conf_level=0.95, interval_type="lower", bias_adj=True, distance=distance, actual_distance=actual_distance)
-        for correlation, size, distance, actual_distance in [
-            (0.05, 274, 0.10, 0.099834696),
-            (0.10, 272, 0.10, 0.099953148),
-            (0.15, 268, 0.10, 0.099943989),
-            (0.20, 262, 0.10, 0.099810938),
-            (0.25, 252, 0.10, 0.099966753),
-            (0.30, 241, 0.10, 0.099839661),
-            (0.35, 227, 0.10, 0.099878563),
-            (0.40, 211, 0.10, 0.099927887),
-            (0.45, 194, 0.10, 0.099790214),
-            (0.50, 175, 0.10, 0.099799846),
-            (0.55, 155, 0.10, 0.099788059),
-            (0.60, 134, 0.10, 0.099902876),
-            (0.65, 113, 0.10, 0.099939493),
-            (0.70, 93, 0.10, 0.099507075),
-            (0.75, 73, 0.10, 0.099388989),
-            (0.80, 54, 0.10, 0.099497394),
-            (0.85, 37, 0.10, 0.099515798),
-            (0.90, 23, 0.10, 0.097919260),
-            (0.95, 12, 0.10, 0.092586343),
+        # correlation = 0.05 to 0.95 by 0.05, conf_level = 0.95, alternative = "lower", distance = 0.10, bias_adj = True
+        TestCase(correlation=correlation, size=size, conf_level=0.95, interval_type="lower", bias_adj=True, distance=0.10, actual_distance=actual_distance)
+        for correlation, size, actual_distance in [
+            (0.05, 274, 0.099834696),
+            (0.10, 272, 0.099953148),
+            (0.15, 268, 0.099943989),
+            (0.20, 262, 0.099810938),
+            (0.25, 252, 0.099966753),
+            (0.30, 241, 0.099839661),
+            (0.35, 227, 0.099878563),
+            (0.40, 211, 0.099927887),
+            (0.45, 194, 0.099790214),
+            (0.50, 175, 0.099799846),
+            (0.55, 155, 0.099788059),
+            (0.60, 134, 0.099902876),
+            (0.65, 113, 0.099939493),
+            (0.70, 93, 0.099507075),
+            (0.75, 73, 0.099388989),
+            (0.80, 54, 0.099497394),
+            (0.85, 37, 0.099515798),
+            (0.90, 23, 0.097919260),
+            (0.95, 12, 0.092586343),
         ]
     ]
 )
@@ -184,26 +183,24 @@ case_group = case_group_not_bias_adj + case_group_bias_adj
 def test_solve_distance(case: TestCase) -> None:
 
     if case.bias_adj:
-        pytest.skip("Currently no test cases available for comparison.")
+        pytest.skip(reason="Neither SAS or PASS don't support bias adjustment, skipping the 'bias_adj = True' test case.")
 
-    assert (
-        round(
-            solve_distance(
-                correlation=case.correlation,
-                size=case.size,
-                conf_level=case.conf_level,
-                interval_type=case.interval_type,
-                bias_adj=case.bias_adj,
-            ),
-            9,
-        )
-        == case.actual_distance
-    )
+    assert round(
+        solve_distance(
+            correlation=case.correlation,
+            size=case.size,
+            conf_level=case.conf_level,
+            interval_type=case.interval_type,
+            bias_adj=case.bias_adj,
+        ),
+        6,
+    ) == round(case.actual_distance, 6)
 
 
 def test_solve_size(case: TestCase) -> None:
+
     if case.bias_adj:
-        pytest.skip("Currently no test cases available for comparison.")
+        pytest.skip(reason="Neither SAS or PASS don't support bias adjustment, skipping the 'bias_adj = True' test case.")
 
     assert (
         solve_size(
@@ -215,28 +212,3 @@ def test_solve_size(case: TestCase) -> None:
         )
         == case.size
     )
-
-
-def test_solve_correlation(case: TestCase) -> None:
-    if case.bias_adj:
-        pytest.skip("Currently no test cases available for comparison.")
-
-    res = solve_correlation(
-        distance=case.actual_distance,
-        size=case.size,
-        conf_level=case.conf_level,
-        interval_type=case.interval_type,
-        bias_adj=case.bias_adj,
-    )
-
-    if isinstance(res, float):
-        assert round(res, 2) == case.correlation
-    elif isinstance(res, tuple):
-        assert round(res[0], 2) == case.correlation or round(res[1], 2) == case.correlation
-    else:
-        assert False
-
-
-def test_solve_correlation_exception() -> None:
-    with pytest.raises(SolutionNotFoundError):
-        solve_correlation(distance=0.1, size=274, conf_level=0.95, interval_type="lower", bias_adj=False)
