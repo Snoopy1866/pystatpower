62d8e64ed14320c961382aba73fcf1c6d52f03fa
Firefly <45487685+Snoopy1866@users.noreply.github.com>
2026-07-06 13:41:30 +0800

    refactor(correlation.inequality): remove adjust method and optimize the docstring

--- src/pystatpower/correlation/inequality.py
+++ src/pystatpower/correlation/inequality.py
@@ -1,75 +1,8 @@
-from math import ceil, log, sqrt
+from math import atanh, ceil, sqrt
 from typing import Literal
 
 from scipy.optimize import brentq
 from scipy.stats import norm
-
-from .._constant import SAMPLE_SIZE_SEARCH_MAX
-
-
-def _power_not_adjusted(
-    null_correlation: float,
-    correlation: float,
-    alternative: Literal["two-sided", "greater", "less"],
-    size: float,
-    alpha: float,
-) -> float:
-    """Calculate the statistical power of the difference test between two correlation coefficients, not adjusted for bias."""
-
-    null_zeta = 1 / 2 * log((1 + null_correlation) / (1 - null_correlation))
-    zeta = 1 / 2 * log((1 + correlation) / (1 - correlation))
-
-    match alternative:
-        case "two-sided":
-            power = (
-                1
-                - norm.cdf(norm.ppf(1 - alpha / 2) - (zeta - null_zeta) * sqrt(size - 3))
-                + norm.cdf(norm.ppf(alpha / 2) - (zeta - null_zeta) * sqrt(size - 3))
-            )
-        case "greater":
-            power = 1 - norm.cdf(norm.ppf(1 - alpha) - (zeta - null_zeta) * sqrt(size - 3))
-        case "less":
-            power = norm.cdf(norm.ppf(alpha) - (zeta - null_zeta) * sqrt(size - 3))
-
-    return float(power)
-
-
-def _power_adjusted(
-    null_correlation: float,
-    correlation: float,
-    alternative: Literal["two-sided", "greater", "less"],
-    size: float,
-    alpha: float,
-) -> float:
-    """Calculate the statistical power of the difference test between two correlation coefficients, adjusted for bias."""
-    null_zeta = 1 / 2 * log((1 + null_correlation) / (1 - null_correlation))
-    zeta = 1 / 2 * log((1 + correlation) / (1 - correlation))
-
-    match alternative:
-        case "two-sided":
-            power = (
-                1
-                - norm.cdf(
-                    norm.ppf(1 - alpha / 2)
-                    - (zeta - null_zeta + 1 / 2 * (correlation - null_correlation) / (size - 1)) * sqrt(size - 3)
-                )
-                + norm.cdf(
-                    norm.ppf(alpha / 2)
-                    - (zeta - null_zeta + 1 / 2 * (correlation - null_correlation) / (size - 1)) * sqrt(size - 3)
-                )
-            )
-        case "greater":
-            power = 1 - norm.cdf(
-                norm.ppf(1 - alpha)
-                - (zeta - null_zeta + 1 / 2 * (correlation - null_correlation) / (size - 1)) * sqrt(size - 3)
-            )
-        case "less":
-            power = norm.cdf(
-                norm.ppf(alpha)
-                - (zeta - null_zeta + 1 / 2 * (correlation - null_correlation) / (size - 1)) * sqrt(size - 3)
-            )
-
-    return float(power)
 
 
 def _power(
@@ -78,14 +11,26 @@ def _power(
     alternative: Literal["two-sided", "greater", "less"],
     size: float,
     alpha: float,
-    bias_adj: bool,
 ) -> float:
-    """Calculate the statistical power of the difference test between two correlation coefficients."""
+    """Calculate the statistical power."""
 
-    if bias_adj:
-        return _power_adjusted(null_correlation, correlation, alternative, size, alpha)
-    else:
-        return _power_not_adjusted(null_correlation, correlation, alternative, size, alpha)
+    null_zeta = atanh(null_correlation)
+    zeta = atanh(correlation)
+    se_recip = sqrt(size - 3)
+
+    match alternative:
+        case "two-sided":
+            power = (
+                1
+                - norm.cdf(norm.ppf(1 - alpha / 2) - (zeta - null_zeta) * se_recip)
+                + norm.cdf(norm.ppf(alpha / 2) - (zeta - null_zeta) * se_recip)
+            )
+        case "greater":
+            power = 1 - norm.cdf(norm.ppf(1 - alpha) - (zeta - null_zeta) * se_recip)
+        case "less":
+            power = norm.cdf(norm.ppf(alpha) - (zeta - null_zeta) * se_recip)
+
+    return float(power)
 
 
 def solve_power(
@@ -95,38 +40,33 @@ def solve_power(
     size: int,
     alternative: Literal["two-sided", "greater", "less"] = "two-sided",
     alpha: float = 0.05,
-    bias_adj: bool = False,
 ) -> float:
-    """Calculate the statistical power of the difference test between two correlation coefficients.
+    """Calculate the statistical power.
 
     Args:
-        null_correlation (float):
+        null_correlation:
             Correlation coefficient under the null hypothesis.
-        correlation (float):
+        correlation:
             Correlation coefficient under the alternative hypothesis.
-        size (int):
+        size:
             Sample size.
-        alternative (Literal["two-sided", "greater", "less"], optional):
-            Type of alternative hypothesis.
+        alternative:
+            Type of the alternative hypothesis.
 
-            - `'two-sided'`: Two-sided alternative hypothesis: $\\rho_1 \\neq \\rho_0$
-            - `'greater'`: Upper one-sided alternative hypothesis: $\\rho_1 > \\rho_0$
-            - `'less'`: Lower one-sided alternative hypothesis: $\\rho_1 < \\rho_0$
-        alpha (float, optional):
+            - If `alternative` is `'two-sided'`, the alternative hypothesis is $\\rho \\neq \\rho_0$.
+            - If `alternative` is `'greater'`, the alternative hypothesis is $\\rho > \\rho_0$.
+            - If `alternative` is `'less'`, the alternative hypothesis is $\\rho < \\rho_0$.
+        alpha:
             Significance level.
 
-            - If `alternative` is `'two-sided'`, provide the two-sided significance level.
-            - If `alternative` is `'greater'` or `'less'`, provide the one-sided significance level.
-        bias_adj (bool, optional):
-            Specify whether the bias adjustment is used or not.
+            - If `alternative` is `'two-sided'`, `alpha` represents the two-sided significance level.
+            - If `alternative` is `'greater'` or `'less'`, `alpha` represents the one-sided significance level.
 
     Returns:
-        (float): Power of the test.
+        The statistical power of the test.
     """
 
-    power = _power(null_correlation, correlation, alternative, size, alpha, bias_adj)
-
-    return float(power)
+    return _power(null_correlation, correlation, alternative, size, alpha)
 
 
 def solve_size(
@@ -135,42 +75,41 @@ def solve_size(
     correlation: float,
     alternative: Literal["two-sided", "greater", "less"] = "two-sided",
     alpha: float = 0.05,
-    power: float = 0.80,
-    bias_adj: bool = False,
+    power: float = 0.8,
 ) -> int:
-    """Estimate the required sample size for the difference test between two correlation coefficients.
+    """Estimate the required sample size.
 
     Args:
-        null_correlation (float):
+        null_correlation:
             Correlation coefficient under the null hypothesis.
-        correlation (float):
+        correlation:
             Correlation coefficient under the alternative hypothesis.
-        alternative (Literal["two-sided", "greater", "less"], optional):
-            Type of alternative hypothesis.
+        size:
+            Sample size.
+        alternative:
+            Type of the alternative hypothesis.
 
-            - `'two-sided'`: Two-sided alternative hypothesis: $\\rho_1 \\neq \\rho_0$
-            - `'greater'`: Upper one-sided alternative hypothesis: $\\rho_1 > \\rho_0$
-            - `'less'`: Lower one-sided alternative hypothesis: $\\rho_1 < \\rho_0$
-        alpha (float, optional):
+            - If `alternative` is `'two-sided'`, the alternative hypothesis is $\\rho \\neq \\rho_0$.
+            - If `alternative` is `'greater'`, the alternative hypothesis is $\\rho > \\rho_0$.
+            - If `alternative` is `'less'`, the alternative hypothesis is $\\rho < \\rho_0$.
+        alpha:
             Significance level.
 
-            - If `alternative` is `'two-sided'`, provide the two-sided significance level.
-            - If `alternative` is `'greater'` or `'less'`, provide the one-sided significance level.
-        power (float, optional):
-            Power of the test.
-        bias_adj (bool, optional):
-            Specify whether the bias adjustment is used or not.
+            - If `alternative` is `'two-sided'`, `alpha` represents the two-sided significance level.
+            - If `alternative` is `'greater'` or `'less'`, `alpha` represents the one-sided significance level.
+        power:
+            Expected statistical power.
+
+            0.8 is a commonly used statistical power.
 
     Returns:
-        (int): The required sample size.
+        The required sample size.
     """
 
     def func(size: float) -> float:
-        return _power(null_correlation, correlation, alternative, size, alpha, bias_adj) - power
+        return _power(null_correlation, correlation, alternative, size, alpha) - power
 
-    size = brentq(func, 3, SAMPLE_SIZE_SEARCH_MAX)
-
-    return ceil(size)
+    return ceil(brentq(func, 3, 1e12))
 
 
 def solve_correlation(
@@ -179,52 +118,66 @@ def solve_correlation(
     size: int,
     alternative: Literal["two-sided", "greater", "less"] = "two-sided",
     alpha: float = 0.05,
-    power: float = 0.80,
-    bias_adj: bool = False,
-    search_direction: Literal["above", "below"] = "above",
+    power: float = 0.8,
+    direction: Literal["greater", "less"] | None = None,
 ) -> float:
     """
-    Estimate the required correlation coefficient under the alternative hypothesis for the difference test between two correlation coefficients.
+    Estimate the required correlation coefficient under the alternative hypothesis.
 
     Args:
-        null_correlation (float):
+        null_correlation:
             Correlation coefficient under the null hypothesis.
-        size (int):
+        size:
             Sample size.
-        alternative (Literal["two-sided", "greater", "less"], optional):
-            Type of alternative hypothesis.
+        alternative:
+            Type of the alternative hypothesis.
 
-            - `'two-sided'`: Two-sided alternative hypothesis: $\\rho_1 \\neq \\rho_0$
-            - `'greater'`: Upper one-sided alternative hypothesis: $\\rho_1 > \\rho_0$
-            - `'less'`: Lower one-sided alternative hypothesis: $\\rho_1 < \\rho_0$
-        alpha (float, optional):
+            - If `alternative` is `'two-sided'`, the alternative hypothesis is $\\rho \\neq \\rho_0$.
+            - If `alternative` is `'greater'`, the alternative hypothesis is $\\rho > \\rho_0$.
+            - If `alternative` is `'less'`, the alternative hypothesis is $\\rho < \\rho_0$.
+        alpha:
             Significance level.
 
-            - If `alternative` is `'two-sided'`, provide the two-sided significance level.
-            - If `alternative` is `'greater'` or `'less'`, provide the one-sided significance level.
-        power (float, optional):
-            Power of the test.
-        bias_adj (bool, optional):
-            Specify whether the bias adjustment is used or not.
-        search_direction (Literal["above", "below"], optional):
-            Specify whether to search for the alternative correlation below or above the null correlation.
+            - If `alternative` is `'two-sided'`, `alpha` represents the two-sided significance level.
+            - If `alternative` is `'greater'` or `'less'`, `alpha` represents the one-sided significance level.
+        power:
+            Expected statistical power.
+
+            0.8 is a commonly used statistical power.
+        direction:
+            The direction for the correlation coefficient under the alternative hypothesis relative to the correlation coefficient under the null hypothesis.
+
+            - `'greater'`: Search for the alternative correlation coefficient greater than the null correlation coefficient.
+            - `'less'`: Search for the alternative correlation coefficient less than the null correlation coefficient.
+
+            !!! note
+                - If `alternative` is `'two-sided'`, this parameter is required.
+                - If `alternative` is `'greater'`, the search direction is automatically inferred to be `'greater'`, and this parameter is ignored.
+                - If `alternative` is `'less'`, the search direction is automatically inferred to be `'less'`, and this parameter is ignored.
 
     Returns:
-        (float): required correlation coefficient under the alternative hypothesis.
+        required correlation coefficient under the alternative hypothesis.
+
+    Raises:
+        ValueError: If `alternative` is `'two-sided'` but `direction` is not specified.
     """
 
+    if alternative == "two-sided":
+        if direction is None:
+            raise ValueError("'direction' is required when 'alternative' is 'two-sided'.")
+    elif alternative == "greater":
+        direction = "greater"
+    else:  # alternative == "less"
+        direction = "less"
+
     def func(correlation: float) -> float:
-        return _power(null_correlation, correlation, alternative, size, alpha, bias_adj) - power
+        return _power(null_correlation, correlation, alternative, size, alpha) - power
 
-    match search_direction:
-        case "below":
-            correlation = brentq(func, -0.999999, null_correlation)
-        case "above":
-            correlation = brentq(func, null_correlation, 0.999999)
-        case _:
-            raise ValueError("search_direction must be 'below' or 'above'")
-
-    return correlation
+    match direction:
+        case "less":
+            return float(brentq(func, -1 + 1e-12, null_correlation))
+        case "greater":
+            return float(brentq(func, null_correlation, 1 - 1e-12))
 
 
 def solve_null_correlation(
@@ -233,49 +186,63 @@ def solve_null_correlation(
     size: int,
     alternative: Literal["two-sided", "greater", "less"] = "two-sided",
     alpha: float = 0.05,
-    power: float = 0.80,
-    bias_adj: bool = False,
-    search_direction: Literal["above", "below"] = "below",
+    power: float = 0.8,
+    direction: Literal["greater", "less"] | None = None,
 ) -> float:
     """
-    Estimete the required correlation coefficient under the null hypothesis for the difference test between two correlation coefficients.
+    Estimete the required correlation coefficient under the alternative hypothesis.
 
     Args:
-        correlation (float):
+        correlation:
             Correlation coefficient under the alternative hypothesis.
-        size (int):
+        size:
             Sample size.
-        alternative (Literal["two-sided", "greater", "less"], optional):
-            Type of alternative hypothesis.
+        alternative:
+            Type of the alternative hypothesis.
 
-            - `'two-sided'`: Two-sided alternative hypothesis: $\\rho_1 \\neq \\rho_0$
-            - `'greater'`: Upper one-sided alternative hypothesis: $\\rho_1 > \\rho_0$
-            - `'less'`: Lower one-sided alternative hypothesis: $\\rho_1 < \\rho_0$
-        alpha (float, optional):
-            Two-sided significance level.
+            - If `alternative` is `'two-sided'`, the alternative hypothesis is $\\rho \\neq \\rho_0$.
+            - If `alternative` is `'greater'`, the alternative hypothesis is $\\rho > \\rho_0$.
+            - If `alternative` is `'less'`, the alternative hypothesis is $\\rho < \\rho_0$.
+        alpha:
+            Significance level.
 
-            - If `alternative` is `'two-sided'`, provide the two-sided significance level.
-            - If `alternative` is `'less'` or `'greater'`, provide the one-sided significance level.
-        power (float, optional):
-            Power of the test.
-        bias_adj (bool, optional):
-            Specify whether the bias adjustment is used or not.
-        search_direction (Literal["above", "below"], optional):
-            Specify whether to search for the null correlation below or above the alternative correlation.
+            - If `alternative` is `'two-sided'`, `alpha` represents the two-sided significance level.
+            - If `alternative` is `'greater'` or `'less'`, `alpha` represents the one-sided significance level.
+        power:
+            Expected statistical power.
+
+            0.8 is a commonly used statistical power.
+        direction:
+            The direction for the correlation coefficient under the null hypothesis relative to the correlation coefficient under the alternative hypothesis.
+
+            - `'greater'`: Search for the null correlation coefficient greater than the alternative correlation coefficient.
+            - `'less'`: Search for the null correlation coefficient less than the alternative correlation coefficient.
+
+            !!! note
+                - If `alternative` is `'two-sided'`, this parameter is required.
+                - If `alternative` is `'greater'`, the search direction is automatically inferred to be `'less'`, and this parameter is ignored.
+                - If `alternative` is `'less'`, the search direction is automatically inferred to be `'greater'`, and this parameter is ignored.
 
     Returns:
-        (float): The required correlation coefficient under the null hypothesis.
+        The required correlation coefficient under the null hypothesis.
+
+    Raises:
+        ValueError: If `alternative` is `'two-sided'` but `direction` is not specified.
     """
 
+    if alternative == "two-sided":
+        if direction is None:
+            raise ValueError("'direction' is required when 'alternative' is 'two-sided'.")
+    elif alternative == "greater":
+        direction = "less"
+    else:  # alternative == "less"
+        direction = "greater"
+
     def func(null_correlation: float) -> float:
-        return _power(null_correlation, correlation, alternative, size, alpha, bias_adj) - power
+        return _power(null_correlation, correlation, alternative, size, alpha) - power
 
-    match search_direction:
-        case "below":
-            null_correlation = brentq(func, -0.999999, correlation)
-        case "above":
-            null_correlation = brentq(func, correlation, 0.999999)
-        case _:
-            raise ValueError("search_direction must be 'below' or 'above'")
-
-    return null_correlation
+    match direction:
+        case "greater":
+            return float(brentq(func, correlation, 1 - 1e-12))
+        case "less":
+            return float(brentq(func, -1 + 1e-12, correlation))
