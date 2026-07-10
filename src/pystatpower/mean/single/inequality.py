72c78b903b1d2ca89ff472d6894cad9bb41258ca
wtwang <45487685+Snoopy1866@users.noreply.github.com>
2026-07-10 15:50:42 +0800

    refactor(mean.single.inequality): optimize algorithm logic (#511)

--- src/pystatpower/mean/single/inequality.py
+++ src/pystatpower/mean/single/inequality.py
@@ -1,68 +1,57 @@
-from math import ceil, sqrt
+import warnings
+
+from math import ceil
 from typing import Literal
 
 from scipy.optimize import brentq
-from scipy.stats import nct, norm, t
+
+from ._power import _power as _raw_power
 
 
-def _verify_mean_and_get_diff(
-    mean: float | None,
-    null_mean: float | None,
-    diff: float | None,
-) -> float:
+class _ParamsValidator:
+    def __init__(
+        self,
+        *,
+        mean: float | None = None,
+        null_mean: float | None = None,
+        diff: float | None = None,
+    ) -> None:
+        self.mean = mean
+        self.null_mean = null_mean
+        self.diff = diff
 
-    if diff is None:
-        if mean is None or null_mean is None:
-            raise ValueError("When 'diff' is omitted, both 'mean' and 'null_mean' are required.")
-        diff = mean - null_mean
+        self.params_provided = {k for k, v in self.__dict__.items() if v is not None and k != "alternative"}
 
-    return diff
+    def validate(self, target: Literal["diff"], *, warning: bool = True) -> None:
+        match target:
+            case "diff":
+                self._validate_against_diff(warning=warning)
 
+    def _validate_against_diff(self, *, warning: bool = True) -> None:
+        if self.diff is not None:
+            params_used = {"diff"}
+        elif self.mean is not None and self.null_mean is not None:
+            self.diff = self.mean - self.null_mean
+            params_used = {"mean", "null_mean"}
 
-def _power_z(
-    diff: float,
-    std: float,
-    size: float,
-    alternative: Literal["two-sided", "greater", "less"],
-    alpha: float,
-) -> float:
-    """Calculate the statistical power, using z-test."""
+        if self.diff is None:
+            raise ValueError(
+                "The mean difference cannot be calculated using the specified combination of parameters. Please provide the 'diff' parameter directly, or use both 'mean' and 'null_mean'."
+            )
 
-    se = std / sqrt(size)
+        if warning:
+            params_redundant = self.params_provided - params_used
+            if params_redundant:
+                self._warn_params_redundant(params_redundant)
 
-    match alternative:
-        case "two-sided":
-            power = 1 - norm.cdf(norm.ppf(1 - alpha / 2) - diff / se) + norm.cdf(norm.ppf(alpha / 2) - diff / se)
-        case "greater":
-            power = 1 - norm.cdf(norm.ppf(1 - alpha) - diff / se)
-        case "less":
-            power = norm.cdf(norm.ppf(alpha) - diff / se)
+    def _warn_params_redundant(self, params: set[str], /) -> None:
+        """Warn if parameters are redundant."""
 
-    return float(power)
-
-
-def _power_t(
-    diff: float,
-    std: float,
-    size: float,
-    alternative: Literal["two-sided", "greater", "less"],
-    alpha: float,
-) -> float:
-    """Calculate the statistical power, using t-test."""
-
-    df = size - 1
-    se = std / sqrt(size)
-    nc = diff / se
-
-    match alternative:
-        case "two-sided":
-            power = 1 - nct.cdf(t.ppf(1 - alpha / 2, df), df, nc) + nct.cdf(t.ppf(alpha / 2, df), df, nc)
-        case "greater":
-            power = 1 - nct.cdf(t.ppf(1 - alpha, df), df, nc)
-        case "less":
-            power = nct.cdf(t.ppf(alpha, df), df, nc)
-
-    return float(power)
+        params_str = ", ".join(params)
+        warnings.warn(
+            f"Redundant parameters detected: {params_str}.",
+            stacklevel=2,
+        )
 
 
 def _power(
@@ -75,13 +64,7 @@ def _power(
 ) -> float:
     """Calculate the statistical power."""
 
-    match dist:
-        case "z":
-            power = _power_z(diff, std, size, alternative, alpha)
-        case "t":
-            power = _power_t(diff, std, size, alternative, alpha)
-
-    return power
+    return _raw_power(diff, std, size, alternative, alpha, dist)
 
 
 def solve_power(
@@ -102,15 +85,15 @@ def solve_power(
         mean:
             Mean under the alternative hypothesis.
 
-            If `diff` is omitted, this parameter and `null_mean` are required.
+            This parameter must be used in conjuction with `null_mean` to compute the `diff`, where `diff` = `mean` - `null_mean`.
         null_mean:
             Mean under the null hypothesis.
 
-            If `diff` is omitted, this parameter and `mean` are required.
+            This parameter must be used in conjuction with `mean` to compute the `diff`, where `diff` = `mean` - `null_mean`.
         diff:
             Mean difference between the alternative hypothesis and the null hypothesis.
 
-            If both `mean` and `null_mean` are not specified, this parameter is required.
+            If this parameter is specified, both `mean` and `null_mean` are ignored.
         std:
             Standard deviation.
         size:
@@ -136,10 +119,12 @@ def solve_power(
         The statistical power of the test.
 
     Raises:
-        ValueError: If all `diff`, `mean` and `null_mean` are omitted.
+        ValueError: The given set of parameters is insufficient to determine the mean difference.
     """
 
-    diff = _verify_mean_and_get_diff(mean, null_mean, diff)
+    pv = _ParamsValidator(mean=mean, null_mean=null_mean, diff=diff)
+    pv.validate(target="diff")
+    diff = pv.diff
 
     return _power(diff, std, size, alternative, alpha, dist)
 
@@ -162,15 +147,15 @@ def solve_size(
         mean:
             Mean under the alternative hypothesis.
 
-            If `diff` is omitted, this parameter and `null_mean` are required.
+            This parameter must be used in conjuction with `null_mean` to compute the `diff`, where `diff` = `mean` - `null_mean`.
         null_mean:
             Mean under the null hypothesis.
 
-            If `diff` is omitted, this parameter and `mean` are required.
+            This parameter must be used in conjuction with `mean` to compute the `diff`, where `diff` = `mean` - `null_mean`.
         diff:
             Mean difference between the alternative hypothesis and the null hypothesis.
 
-            If both `mean` and `null_mean` are not specified, this parameter is required.
+            If this parameter is specified, both `mean` and `null_mean` are ignored.
         std:
             Standard deviation.
         alternative:
@@ -187,7 +172,7 @@ def solve_size(
         power:
             Expected statistical power.
 
-            0.8 is a commonly used statistical power.
+            0.8 is a commonly used value for statistical power.
         dist:
             The distribution used for the test.
 
@@ -198,89 +183,17 @@ def solve_size(
         The required sample size.
 
     Raises:
-        ValueError: If all `diff`, `mean` and `null_mean` are omitted.
+        ValueError: The given set of parameters is insufficient to determine the mean difference.
     """
 
-    diff = _verify_mean_and_get_diff(mean, null_mean, diff)
+    pv = _ParamsValidator(mean=mean, null_mean=null_mean, diff=diff)
+    pv.validate(target="diff")
+    diff = pv.diff
 
     def func(size: float) -> float:
         return _power(diff, std, size, alternative, alpha, dist) - power
 
     return ceil(brentq(func, 1 + 0.1, 1e12))
-
-
-def solve_diff(
-    *,
-    std: float,
-    size: int,
-    alternative: Literal["two-sided", "greater", "less"],
-    alpha: float,
-    power: float,
-    dist: Literal["z", "t"],
-    direction: Literal["greater", "less"] | None = None,
-) -> float:
-    """
-    Estimate the required mean difference.
-
-    Args:
-        std:
-            Standard deviation.
-        size:
-            Sample size.
-        alternative:
-            Type of the alternative hypothesis.
-
-            - If `alternative` is `'two-sided'`, the alternative hypothesis is $\\mu \\neq \\mu_0$
-            - If `alternative` is `'greater'`, the alternative hypothesis is $\\mu > \\mu_0$
-            - If `alternative` is `'less'`, the alternative hypothesis is $\\mu < \\mu_0$
-        alpha:
-            Significance level.
-
-            - If `alternative` is `'two-sided'`, `alpha` represents the two-sided significance level.
-            - If `alternative` is `'greater'` or `'less'`, `alpha` represents the one-sided significance level.
-        power:
-            Expected statistical power.
-
-            0.8 is a commonly used statistical power.
-        dist:
-            The distribution used for the test.
-
-            - `'z'`: Normal distribution.
-            - `'t'`: Student's t distribution.
-        direction:
-            The search direction for the mean difference relative to zero.
-
-            - `'greater'`: Search for the mean difference that is greater than zero.
-            - `'less'`: Search for the mean difference that is less than zero.
-
-            !!! note
-                - If `alternative` is `'two-sided'`, the parameter `direction` is required.
-                - If `alternative` is `'greater'`, the search direction is automatically inferred to be `'greater'`, and the parameter `direction` is ignored.
-                - If `alternative` is `'less'`, the search direction is automatically inferred to be `'less'`, and the parameter `direction` is ignored.
-
-    Returns:
-        The required mean difference between the alternative hypothesis and the null hypothesis.
-
-    Raises:
-        ValueError: If `alternative` is `'two-sided'` but `direction` is omitted.
-    """
-
-    if alternative == "two-sided":
-        if direction is None:
-            raise ValueError("'direction' is required when 'alternative' is 'two-sided'.")
-    elif alternative == "greater":
-        direction = "greater"
-    else:  # alternative == "less"
-        direction = "less"
-
-    def func(diff: float) -> float:
-        return _power(diff, std, size, alternative, alpha, dist) - power
-
-    match direction:
-        case "greater":
-            return float(brentq(func, 0, 1e9))
-        case "less":
-            return float(brentq(func, -1e9, 0))
 
 
 def solve_mean(
@@ -318,7 +231,7 @@ def solve_mean(
         power:
             Expected statistical power.
 
-            0.8 is a commonly used statistical power.
+            0.8 is a commonly used value for statistical power.
         dist:
             The distribution used for the test.
 
@@ -339,7 +252,7 @@ def solve_mean(
         The required mean under the alternative hypothesis.
 
     Raises:
-        ValueError: If `alternative` is `'two-sided'` but `direction` is omitted.
+        ValueError: If `alternative` is `'two-sided'` and `direction` is omitted.
     """
 
     if alternative == "two-sided":
@@ -395,7 +308,7 @@ def solve_null_mean(
         power:
             Expected statistical power.
 
-            0.8 is a commonly used statistical power.
+            0.8 is a commonly used value for statistical power.
         dist:
             The distribution used for the test.
 
@@ -416,7 +329,7 @@ def solve_null_mean(
         The required mean under the null hypothesis.
 
     Raises:
-        ValueError: If `alternative` is `'two-sided'` but `direction` is omitted.
+        ValueError: If `alternative` is `'two-sided'` and `direction` is omitted.
     """
 
     if alternative == "two-sided":
@@ -437,6 +350,80 @@ def solve_null_mean(
             return float(brentq(func, -1e9, mean))
 
 
+def solve_diff(
+    *,
+    std: float,
+    size: int,
+    alternative: Literal["two-sided", "greater", "less"],
+    alpha: float,
+    power: float,
+    dist: Literal["z", "t"],
+    direction: Literal["greater", "less"] | None = None,
+) -> float:
+    """
+    Estimate the required mean difference.
+
+    Args:
+        std:
+            Standard deviation.
+        size:
+            Sample size.
+        alternative:
+            Type of the alternative hypothesis.
+
+            - If `alternative` is `'two-sided'`, the alternative hypothesis is $\\mu \\neq \\mu_0$
+            - If `alternative` is `'greater'`, the alternative hypothesis is $\\mu > \\mu_0$
+            - If `alternative` is `'less'`, the alternative hypothesis is $\\mu < \\mu_0$
+        alpha:
+            Significance level.
+
+            - If `alternative` is `'two-sided'`, `alpha` represents the two-sided significance level.
+            - If `alternative` is `'greater'` or `'less'`, `alpha` represents the one-sided significance level.
+        power:
+            Expected statistical power.
+
+            0.8 is a commonly used value for statistical power.
+        dist:
+            The distribution used for the test.
+
+            - `'z'`: Normal distribution.
+            - `'t'`: Student's t distribution.
+        direction:
+            The search direction for the mean difference relative to zero.
+
+            - `'greater'`: Search for the mean difference that is greater than zero.
+            - `'less'`: Search for the mean difference that is less than zero.
+
+            !!! note
+                - If `alternative` is `'two-sided'`, the parameter `direction` is required.
+                - If `alternative` is `'greater'`, the search direction is automatically inferred to be `'greater'`, and the parameter `direction` is ignored.
+                - If `alternative` is `'less'`, the search direction is automatically inferred to be `'less'`, and the parameter `direction` is ignored.
+
+    Returns:
+        The required mean difference between the alternative hypothesis and the null hypothesis.
+
+    Raises:
+        ValueError: If `alternative` is `'two-sided'` and `direction` is omitted.
+    """
+
+    if alternative == "two-sided":
+        if direction is None:
+            raise ValueError("'direction' is required when 'alternative' is 'two-sided'.")
+    elif alternative == "greater":
+        direction = "greater"
+    else:  # alternative == "less"
+        direction = "less"
+
+    def func(diff: float) -> float:
+        return _power(diff, std, size, alternative, alpha, dist) - power
+
+    match direction:
+        case "greater":
+            return float(brentq(func, 0, 1e9))
+        case "less":
+            return float(brentq(func, -1e9, 0))
+
+
 def solve_std(
     *,
     mean: float | None = None,
@@ -455,15 +442,15 @@ def solve_std(
         mean:
             Mean under the alternative hypothesis.
 
-            If `diff` is omitted, this parameter and `null_mean` are required.
+            This parameter must be used in conjuction with `null_mean` to compute the `diff`, where `diff` = `mean` - `null_mean`.
         null_mean:
             Mean under the null hypothesis.
 
-            If `diff` is omitted, this parameter and `mean` are required.
+            This parameter must be used in conjuction with `mean` to compute the `diff`, where `diff` = `mean` - `null_mean`.
         diff:
             Mean difference between the alternative hypothesis and the null hypothesis.
 
-            If both `mean` and `null_mean` are not specified, this parameter is required.
+            If this parameter is specified, both `mean` and `null_mean` are ignored.
         size:
             Sample size.
         alternative:
@@ -480,7 +467,7 @@ def solve_std(
         power:
             Expected statistical power.
 
-            0.8 is a commonly used statistical power.
+            0.8 is a commonly used value for statistical power.
         dist:
             The distribution used for the test.
 
@@ -491,10 +478,12 @@ def solve_std(
         The required standard deviation.
 
     Raises:
-        ValueError: If all `diff`, `mean` and `null_mean` are omitted.
+        ValueError: The given set of parameters is insufficient to determine the mean difference.
     """
 
-    diff = _verify_mean_and_get_diff(mean, null_mean, diff)
+    pv = _ParamsValidator(mean=mean, null_mean=null_mean, diff=diff)
+    pv.validate(target="diff")
+    diff = pv.diff
 
     def func(std: float) -> float:
         return _power(diff, std, size, alternative, alpha, dist) - power
